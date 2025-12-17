6a85c97cdeea10b7cc7260c04aadf989dcbf45a6
avantikaaa01 <avantikabibhu@gmail.com>
2025-12-17 15:25:55 +0530

    Disable prerendering for API route

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,8 +1,11 @@
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const res = await fetch("https://fakestoreapi.com/products", {
     headers: {
       "User-Agent": "Mozilla/5.0",
     },
+    cache: "no-store",
   });
 
   const data = await res.json();
